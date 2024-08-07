f76457256ab686606b585f866eab41eeb96bf825
Neha Bansal <neha.bansal@internationalsos.com>
2024-08-07 14:40:40 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="app">
+      {/* <div class="wave">
+          <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+            <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" style={{stroke: "none", fill:"black"}}></path>
+          </svg>
+        </div> */}
+      <div className="head-image">
+        <img src={process.env.PUBLIC_URL+'main.jpeg'}/>
+      </div>
+      <div className='main'>
+        <div className="marquee marquee10">
+          <div className="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+          <div aria-hidden="true" class="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+        </div>
+        <div className="marquee marquee20">
+          <div className="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+          <div aria-hidden="true" class="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+        </div>
+        <div className="marquee marquee30">
+          <div className="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+          <div aria-hidden="true" class="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+        </div>
+        <div className="marquee marquee40">
+          <div className="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+          <div aria-hidden="true" class="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+        </div>
+        <div className="marquee marquee50">
+          <div className="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+          <div aria-hidden="true" class="marquee__group">
+            Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence Infinite Marquee with long sentence
+          </div>
+        </div>
+        
+      </div>
+    </section>
   );
 }
 
